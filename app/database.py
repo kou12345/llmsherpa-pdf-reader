23df1a0349468b0b9e171abac95c7d2e2e3479d7
kou12345 <hi.0412.ko.2002@gmail.com>
2024-08-31 19:03:23 +0900

    chore: Update database.py to enable SQLALCHEMY_DATABASE_URL echo

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db:5432/mydatabase"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
